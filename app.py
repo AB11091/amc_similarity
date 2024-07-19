a97fd4dbdbe7d1e5b33a0d97df38142625a51334
Aditya Bharadwaj <aditya09bharadwaj@gmail.com>
2024-07-18 23:30:22 -0700

    organized folders, requirements.txt

--- app.py
+++ app.py
@@ -40,5 +40,4 @@ def get_embedding():
     return [document for document in enumerate(search_results)]
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5500))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
